8c0ccc673832433b075fdde89b1986f664f1d7b4
Yttehs <shetty.yttehs@outlook.com>
2024-12-29 19:10:13 +0800

    chore: change app name

--- android/app/src/main/java/com/rn_counter/MainActivity.kt
+++ android/app/src/main/java/com/rn_counter/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "RN_Counter"
+  override fun getMainComponentName(): String = "RN-Counter"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
